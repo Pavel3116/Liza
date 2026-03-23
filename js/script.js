37cb38b25c9b82cfbc56cdcad43eebb1bad1f572
Pavel <pavlikgena23@gmail.com>
2026-03-23 17:36:23 +0300

    Все почти готово

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
-function convert(amointб, curr){
-    console.log(curr* amoint);
+"use strict"
+
+const arr = [10,4,3,6,8];
+
+arr.pop(); //удаление 
+console.log(arr);
+
+arr.push(23);
+console.log(arr);
+
+for( let i = 0; i< arr.length; i++){
+    console.log(arr[i]);
 }
 
+for(let value of arr){
+    console.log(value);
+}
 
-convert(500, 28);
+// arr[99] = 0;
+console.log(arr.length); //свойство length состоит и индекса последнего элемента + 1
+
+console.log(arr); //[ 1, 2, 3, 6, 23, <94 empty items>, 0 ]
+
+
+// Методы
+
+arr.forEach(function(itemp, i, arr){
+    console.log(`${i}: ${itemp} внутри массива ${arr}`);
+});
+
+const str = prompt("", '');
+const product = str.split(', ');
+console.log(product);
+product.sort();
+
+
+console.log(product.join('; '));
+
+
+function compareNum(a,b){
+    return a-b;
+}
+arr.sort(compareNum);
+console.log(arr);
--- js/script2.js
+++ js/script2.js
@@ -1,16 +1,88 @@
-let numberOfFilms = prompt("Сколько фильмов вы уже поссмотрели?", 0);
+// /* Задание на урок:
 
-let obg = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false,
+// 1) Первую часть задания повторить по уроку
 
-}
-let oldFilm = prompt("Один из последних просмотренных филтмов","");
-let log = prompt("На сколько оцените фильм", 0);
+// 2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+// false - выводит в консоль главный объект программы
 
-obg.movies[oldFilm] = log;
+// 3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+// "Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+// genres
 
-console.log(obg);
+// P.S. Функции вызывать не обязательно */
+
+// 'use strict';
+
+// let numberOfFilms;
+
+// function start() {
+//     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+//     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//         numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//     }
+// }
+
+// start();
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+
+// function rememberMyFilms() {
+//     for (let i = 0; i < 2; i++) {
+//         let a = prompt('Один из последних просмотренных фильмов?', ''),
+//               b = prompt('На сколько оцените его?', '');
+//          a = a.trim();
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//             personalMovieDB.movies[a] = b;
+//             console.log('done');
+//         } else {
+//             console.log('error');
+//             i--;
+//         }
+//     }
+// }
+
+// rememberMyFilms();
+
+// function detectPersonalLevel() {
+//     if (personalMovieDB.count < 10) {
+//         console.log("Просмотрено довольно мало фильмов");
+//     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//         console.log("Вы классический зритель");
+//     } else if (personalMovieDB.count >= 30) {
+//         console.log("Вы киноман");
+//     } else {
+//         console.log("Произошла ошибка");
+//     }
+// }
+
+// detectPersonalLevel();
+
+// function showMyDB (hidden) {
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//     for (let i = 1; i <= 3; i++) {
+//         personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+//     }
+// }
+
+// writeYourGenres();
+// let a;
+// while(a != 'Елизавета' || a!= 'Лиза' ){
+
+//     a = prompt("Привет привет! Введи свое имя!", '');
+// }
+
